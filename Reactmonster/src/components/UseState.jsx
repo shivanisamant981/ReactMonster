bcd0852a6d5f79292be99ba26798a5534aac2b23
shivani <shivanisamant981@gmail.com>
2025-10-24 23:53:17 +0530

    UseState

--- Reactmonster/src/components/UseState.jsx
+++ Reactmonster/src/components/UseState.jsx
@@ -7,12 +7,23 @@ function UseState() {
         name:"conjuring",
         rating:10,
     })
+    const [hmovie,sethmovie]=useState([
+        {id:1 ,name:"conjuring",rating:5},
+        {id:2 ,name:"kanchna",rating:2},
+        {id:3 ,name:"incidious",rating:9},
+        
+    ])
     const handleclick=()=>{
-        const copymovie={
-            ...Movies,
-            rating:5,
-        }
-        setmovie(copymovie);
+        // const copymovie={
+        //     ...Movies,
+        //     rating:5,
+        // }
+
+        setmovie({...Movies,rating:9})
+        // setmovie(copymovie);
+    }
+    const handleEvent=()=>{
+        sethmovie(hmovie.map((h)=>h.id==1?{...h,name:"tumbabd"}:h))
     }
   return (
     <div>
@@ -25,6 +36,18 @@ function UseState() {
         <h1>Name:{Movies.name}</h1>
         <h2>Rating:{Movies.rating}</h2>
         <button onClick={handleclick}>change rating</button>
+        {
+            hmovie.map((hm)=>(
+                <ul> <li key={hm.id}> {hm.id}</li>
+                      <li>{hm.name}</li>
+                      <li>{hm.rating}</li>
+               
+                 </ul>
+                
+            
+            ))
+        }
+        <button onClick={handleEvent}> click me to change the name of the movie</button>
 
       
     </div>
